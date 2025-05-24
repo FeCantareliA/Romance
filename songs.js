48ad5deadca36bd449448b645b630e3ec61127eb
Felipe Notebook <fecantareli@outlook.com>
2025-05-24 13:29:04 -0300

    Player music

---
+++ songs.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    src: "./musics/Lovers Rock [j_sG_Juncn8].mp3",
+    name: "Lovers Rock - TV Girl",
+  },
+  {
+    src: "./musics/Cinema [G9euXtAZ1ME].mp3",
+    name: "Cinema - CASTLEBEAT",
+  },
+  {
+    src: "./musics/Surf Curse - Disco [SOaDmn5Rfzs].mp3",
+    name: "Surf Curse - Disco",
+  },
+  {
+    src: "./musics/Billie Eilish - BIRDS OF A FEATHER (Official Lyric Video) [d5gf9dXbPi0].mp3",
+    name: "BIRDS OF A FEATHER _ Billie Eilish",
+  },
+  
+];
